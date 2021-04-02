3f5e19cf232e12f3e94e464d7bbfb5d1f076ddf4
yxie <yxie@pisx.com>
2021-04-02 15:24:26 +0800

    问题风险链调整提交

--- PiIF/PIIFAPP/src/views/report/IssueRiskChain.vue
+++ PiIF/PIIFAPP/src/views/report/IssueRiskChain.vue
@@ -1,19 +1,11 @@
 <template>
-      <!-- <a-tabs>
-          <a-tab-pane tab="方向性图标" key="1">
-            <div id="container1"></div>
-              <div id="container2"></div>
-          </a-tab-pane>
-          <a-tab-pane tab="方向性图标" key="2">
-          </a-tab-pane>
-      </a-tabs> -->
       <a-card :bordered="false">
         <div class="title">问题风险链</div>
-        <div class="tab-box" v-if="data && data.length && data.length>1">
+        <!-- <div class="tab-box" v-if="data && data.length && data.length>1">
              <div>
                <div v-for="(item,index) in data" :key="index" class="tab-item" :class="{active: activeIdx ==index}" @click="clickTab(item,index)">tab{{index}}</div>
              </div>
-        </div>
+        </div> -->
         <div>
             <div id="container"></div>
         </div>
@@ -23,6 +15,127 @@
 <script>
 import G6 from '@antv/g6';
 import {getAction} from '@/api/manage';
+// const data = {
+//   nodes: [
+//     {
+//       id: '0',
+//       confirm_status: '1',
+//       risk_name: '',
+//       risk_description: 'risk_description'
+//     },
+//     {
+//       id: '1',
+//        confirm_status: '0',
+//       risk_name: '1我当初',
+//     },
+//     {
+//       id: '2',
+//       confirm_status: '1',
+//       risk_name: '2要不',
+//     },
+//     {
+//       id: '3',
+//       confirm_status: '1',
+//       risk_name: '3介绍',
+//     },
+//     {
+//       id: '4',
+//       confirm_status: '0',
+//       risk_name: '444',
+//     },
+//     {
+//       id: '5',
+//       confirm_status: '1',
+//       risk_name: '5介绍5',
+//     },
+//     {
+//       id: '6',
+//       risk_name: '6介绍',
+//     },
+//     {
+//       id: '7',
+//       risk_name: '7介绍',
+//     },
+//     {
+//       id: '8',
+//       risk_name: '8介绍',
+//     },
+//     {
+//       id: '9',
+//       risk_name: '9介绍',
+//     },
+//     {
+//       id: '10',
+//       risk_name: '10te',
+//     }
+//   ],
+//   edges: [
+//     {
+//       source: '0',
+//       target: '1',
+//     },
+//     {
+//       source: '0',
+//       target: '2',
+//     },
+//     {
+//       source: '0',
+//       target: '3',
+//     },
+//     // {
+//     //   source: '0',
+//     //   target: '4',
+//     // },
+//     {
+//       source: '0',
+//       target: '5',
+//     },
+//     {
+//       source: '0',
+//       target: '7',
+//     },
+//     {
+//       source: '0',
+//       target: '8',
+//     },
+//     {
+//       source: '0',
+//       target: '9',
+//     },
+//     {
+//       source: '0',
+//       target: '10',
+//     },
+//     {
+//       source: '2',
+//       target: '3',
+//     },
+//     {
+//       source: '4',
+//       target: '5',
+//     },
+//     {
+//       source: '5',
+//       target: '6',
+//     },
+//     {
+//       source: '6',
+//       target: '7',
+//     },
+//     {
+//       source: '7',
+//       target: '10',
+//     },
+//     {
+//       source: '8',
+//       target: '10',
+//     },
+//     {
+//       source: '9',
+//       target: '10',
+//     },
+//   ],
+// };
   export default {
     name: "testTable",
     data () {
@@ -30,135 +143,7 @@ import {getAction} from '@/api/manage';
         devicetopologyChart: null,
         tooltip: null,
         activeIdx: 0,
-        data: [
-          // {
-          //     id: 'A',
-          //     confirm_status: '1',
-          //     children: [
-          //       {
-          //         id: 'A1',
-          //         confirm_status: '1',
-          //         children: [{ id: 'A11' ,confirm_status: '1',risk_name: 'This is edge from node 0 to node 2.'}, { id: 'A12',confirm_status: '1' ,risk_name: '介绍'}, { id: 'A13',confirm_status: '0',risk_name: '介绍a' }, { id: 'A14',confirm_status: '0',risk_name: '介绍' }],
-          //       },
-          //       {
-          //         id: 'A2',
-          //         confirm_status: '1',
-          //         children: [
-          //           {
-          //             id: 'A21',
-          //             confirm_status: '1',
-          //             risk_name: '介绍',
-          //             children: [{ id: 'A211',confirm_status: '0',risk_name: 'This is edge from node 0 to node 2.' }, { id: 'A212' ,confirm_status: '1'}],
-          //           },
-          //           {
-          //             id: 'A22',
-          //             confirm_status: '0',
-          //             risk_name: '介绍A'
-          //           },
-          //         ],
-          //       },
-          //       {
-          //         id: 'A3',
-          //         confirm_status: '1',
-          //         risk_name: '介绍',
-          //         children: [
-          //           {
-          //             id: 'A31',
-          //             confirm_status: '1',
-          //             children: [{ id: 'A311',confirm_status: '0',risk_name: 'This is edge from' }, { id: 'A312' ,confirm_status: '1'}],
-          //           },
-          //           {
-          //             id: 'A32',
-          //             risk_name: '测试',
-          //             confirm_status: '0',
-          //             children: [
-          //               {
-          //                 id: 'A301',
-          //                 confirm_status: '1',
-          //                 children: [{ id: 'A3011',confirm_status: '0',risk_name: 'This is edge from' }, { id: 'A3120' ,confirm_status: '1'}],
-          //               },
-          //               {
-          //                 id: 'A320',
-          //                 risk_name: '测试',
-          //                 confirm_status: '0',
-          //               },
-          //             ]
-          //           },
-          //         ],
-          //       },
-          //     ],
-          // },
-          // {
-          //     id: 'B',
-          //     confirm_status: '1',
-          //     risk_name: '介绍',
-          //     children: [
-          //       {
-          //         id: 'B1',
-          //         confirm_status: '1',
-          //         children: [{ id: 'B11' ,confirm_status: '1',risk_name: '介绍js'}, { id: 'A12',confirm_status: '    已确认' }, { id: 'A13',confirm_status: '0' }, { id: 'A14',confirm_status: '0' }],
-          //       },
-          //       {
-          //         id: 'A2',
-          //         confirm_status: '1',
-          //         children: [
-          //           {
-          //             id: 'A21',
-          //             confirm_status: '1',
-          //             children: [{ id: 'A211',confirm_status: '0',risk_name: 'This is edge from node 0 to node 2.' }, { id: 'A212' ,confirm_status: '1'}],
-          //           },
-          //           {
-          //             id: 'A22',
-          //             risk_name: '123',
-          //             confirm_status: '0'
-          //           },
-          //         ],
-          //       }
-          //     ],
-          // },
-          //  {
-          //     id: 'C',
-          //     confirm_status: '1',
-          //     risk_name: '介绍C',
-          //     children: [
-          //       {
-          //         id: 'C1',
-          //         confirm_status: '1',
-          //         children: [
-          //           { id: 'B11' ,
-          //           confirm_status: '1',
-          //           risk_name: 'This is edge',
-          //           children: [
-          //              { id: 'C11' ,
-          //               confirm_status: '1',
-          //                risk_name: 'This is edge from node 0 to node 2.',
-          //                children: [
-          //                   { id: 'D11' ,
-          //           confirm_status: '1',
-          //           risk_name: 'This is edge from node 0 to node 2.',
-          //           children: [
-          //              { id: 'E11' ,
-          //           confirm_status: '1',
-          //           risk_name: 'This is edge from node 0 to node 2.'}
-          //           ]
-          //           }
-          //                ]
-          //           }
-          //           ]
-          //           },
-          //            { id: 'A12',confirm_status: '1' }, { id: 'A13',confirm_status: '0' }, { id: 'A14',confirm_status: '0' }],
-          //       },
-          //       {
-          //         id: 'W1',
-          //         confirm_status: '1',
-          //       },
-          //        {
-          //         id: 'q1',
-          //         confirm_status: '1',
-          //       }
-          //     ],
-          // }
-        ],
+        data: [],
         url: {
               tableDate: "/OTDrivice/problemRickChain",
         },
@@ -168,16 +153,21 @@ import {getAction} from '@/api/manage';
          this.getChainData()
          if (typeof window !== 'undefined')
            window.onresize = () => {
-             this.devicetopologyChart && this.devicetopologyChart.destroy();;
-             this.getShape(this.activeIdx+1)
+             this.devicetopologyChart && this.devicetopologyChart.destroy();
+             this.getShape()
            };
-    }, 
+    },
+    watch: {
+      'data': {
+        handler(v){
+          if(v){
+            //  this.devicetopologyChart && this.devicetopologyChart.destroy();
+            //  this.getShape()
+          }
+        }
+      }
+    },
     methods: {
-      clickTab(item,i){
-          this.activeIdx = i
-          this.devicetopologyChart && this.devicetopologyChart.destroy();
-          this.getShape(this.activeIdx+1)
-      },
        getChainData(){
           const  url = this.url.tableDate,_this=this,params={};
           params.riskId = this.$route.query.riskId
@@ -186,10 +176,16 @@ import {getAction} from '@/api/manage';
           getAction(url,this.queryParam,'get').then((res) => {
             _this.isLoading = false
            if(res.success && res.result){
-              _this.dealRes(res.result)
+              res.result.nodes.map(function(item){
+                item.id = String(item.id)
+              })
+              res.result.edges.map(function(item){
+                item.source = String(item.source)
+                item.target = String(item.target)
+              })
               _this.$set(_this,'data',res.result)
               _this.$nextTick(function(){
-                _this.getShape(this.activeIdx+1)
+                _this.getShape()
               })
            }else{
            }
@@ -198,39 +194,19 @@ import {getAction} from '@/api/manage';
               _this.isLoading = false
          })
        },
-       dealRes(data){
-         var str = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-         var _this = this
-         data.map(function(item){
-           var random = str[Math.floor(Math.random() * str.length)] + str[Math.floor(Math.random() * str.length)] 
-           item.id = item.id + random + Math.random()
-           if(item.children && item.children.length){
-               _this.dealRes(item.children)
-           }
-         })
-       },
-       getShape(i){
+       getShape(){
             G6.registerNode('card-node', {
               draw: function drawShape(cfg, group) {
                 const r = 2;
                 const color = '#5B8FF9';
                 const w = cfg.size[0];
                 const h = cfg.size[1];
-                // const colors = {
-                //   0: '#FF424C',
-                //   1: '#40a9ff'
-                // }
-                const textStyle = {
-                  textAlign: 'center',
-                  fontSize: 16,
-                  fill: 'rgba(0, 0, 0, 0.9)'
-                }
                 const shape = group.addShape('rect', {
                   attrs: {
-                    x: -w / 2,
+                    x: -w / 2 ,
                     y: -h / 2,
-                    width: w , //200,
-                    height: h, // 60
+                    width: w , 
+                    height: h, 
                     stroke: color,
                     radius: r,
                     fill: '#fff',
@@ -243,8 +219,8 @@ import {getAction} from '@/api/manage';
                   attrs: {
                     x: -w / 2,
                     y: -h / 2,
-                    width: w, //200,
-                    height: h / 2, // 60
+                    width: w, 
+                    height: h / 2, 
                     // fill: edgeCircleColorArr[0],
                     // fill: colors[cfg.confirm_status],
                     fill: cfg.confirm_status == 1 ? '#40a9ff' : '#FF424C',
@@ -258,10 +234,10 @@ import {getAction} from '@/api/manage';
                 group.addShape('text', {
                   attrs: {
                     textBaseline: 'top',
-                    x: -w / 2 + 8,
+                    x: -w / 2 + 70,
                     y: -h / 2 + 12,
-                    lineHeight: 30,
-                    fontSize: 16,
+                    // lineHeight: 30,
+                    fontSize: 14,
                     text: cfg.confirm_status == 1 ? "已确认" : "未确认",
                     fill: '#fff',
                   },
@@ -271,11 +247,10 @@ import {getAction} from '@/api/manage';
                   attrs: {
                     textBaseline: 'top',
                     x: -w / 2 + 8,
-                    y: -h / 2 + 50,
-                    // text: 'risk_name',
+                    y: -h / 2 + 44,
                     text: cfg.risk_name,
-                    fontSize: 16,
                     fill: 'black',
+                    fontSize: 14,
                   },
                   name: `risk_name`,
                 });
@@ -306,84 +281,74 @@ import {getAction} from '@/api/manage';
                      </ul>`;
                    return outDiv;
                  },
-                //  shouldBegin: (e) => {
-                //    // console.log(e.item);
-                //    let res = true;
-                //    switch (e.item.getModel().id) {
-                //      case '1':
-                //        res = false;
-                //        break;
-                //      case '2':
-                //        if (e.target.get('name') === 'text-shape') res = true;
-                //        else res = false;
-                //        break;
-                //      case '3':
-                //        if (e.target.get('name') !== 'text-shape') res = true;
-                //        else res = false;
-                //        break;
-                //      default:
-                //        res = true;
-                //        break;
-                //    }
-                //    return res;
-                //  },
               });
               this.tooltip = tooltip
-              this.renderChart(tooltip,i)
+              this.renderChart(tooltip)
        },
-       renderChart(tooltip,i){
-        //  const name = "container" + i
-         const container = document.getElementById("container");
-         const width = !this.data[i-1].children || !this.data[i-1].children.length ? 300 : (!this.data[i-1].children.some(function(item){ return item.children }) ? 700 : document.body.clientWidth - 20 ) 
-        //  const width = document.body.clientWidth - 20;
-         const height =  document.body.clientHeight - 120;
-        //  console.log(document.body.clientHeight,container.scrollHeight);
-        const flag = !this.data.children || this.data.children.length == 1 
-         const graph = new G6.TreeGraph({
-           container: container,
-           width,
-           height,
-           linkCenter: true,
-           plugins: [tooltip],
-           modes: {
-            //  default: ['drag-canvas'],
-           },
-           defaultNode: {
-             type: 'card-node',
-             size: [360, 80],
-           },
-           defaultEdge: {
-             type: 'cubic-horizontal',
-             style: {
-               endArrow: true,
-             },
-           },
-           layout: {
-             type: 'indented',
-             direction: 'LR',
-             dropCap: false,
-             indent: 420,
-             getHeight: () => {
-               return 80;
-             },
-           },
+       renderChart(tooltip){
+        //  const container = document.getElementById("container");
+         const width = document.body.scrollWidth - 40;
+         const height =  document.body.clientHeight - 50;
+         console.log(width);
+         const graph = new G6.Graph({
+              container: 'container',
+              width,
+              height,
+              plugins: [tooltip],
+              fitView: true,
+              // controlPoints: false,
+              modes: {
+                default: ['drag-canvas', 'drag-node'],
+              },
+              layout: {
+                rankdir: 'TB',
+                type: 'dagre',
+                // rankdir: 'TB',
+                // align: 'UL',
+                controlPoints: true,
+                // nodesepFunc: () => 20,
+                // ranksepFunc: () => 20,
+              },
+              animate: true,
+              defaultNode: {
+                size: [198, 68],
+                type: 'card-node',
+                style: {
+                  lineWidth: 2,
+                  stroke: '#5B8FF9',
+                  fill: '#C6E5FF',
+                },
+              },
+              defaultEdge: {
+                type: 'polyline',
+                size: 3,
+                color: '#e2e2e2',
+                style: {
+                  endArrow: {
+                    path: 'M 0,0 L 16,8 L 16,-8 Z',
+                    fill: '#e2e2e2',
+                  },
+                  radius: 30,
+                },
+              },
           });
           // graph.clear();
-         this.graphData(graph,i)
+         this.graphData(graph)
        },
-       graphData(graph,i){
-         graph.data(this.data[i-1]);
+       graphData(graph){
+         graph.data(this.data);
          this.devicetopologyChart = graph;
          graph.render();
          graph.fitView();
-         graph.on('node:mouseenter', (e) => {
-           graph.setItemState(e.item, 'active', true);
-         });
-         graph.on('node:mouseleave', (e) => {
-           graph.setItemState(e.item, 'active', false);
-         });
-         }
-       }
+        //  graph.on('node:mouseenter', (e) => {
+        //    graph.setItemState(e.item, 'active', true);
+        //  });
+        //  graph.on('node:mouseleave', (e) => {
+        //    graph.setItemState(e.item, 'active', false);
+        //  });
+        },
+    },
+       
   }
 </script>
 
