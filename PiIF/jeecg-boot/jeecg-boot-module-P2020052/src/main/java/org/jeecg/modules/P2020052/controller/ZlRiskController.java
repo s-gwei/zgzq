1eb5bead06d093bd68876729980c9a1a0e05e30b
zhuli <308020896@qq.com>
2021-04-13 21:00:20 +0800

    提交部门风险系数报表

---
+++ PiIF/jeecg-boot/jeecg-boot-module-P2020052/src/main/java/org/jeecg/modules/P2020052/controller/ZlRiskController.java
@@ -0,0 +1,30 @@
+package org.jeecg.modules.P2020052.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.jeecg.modules.P2020052.service.ZLService;
+import org.jeecg.modules.P2020052.util.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
+
+@RestController
+@RequestMapping("/ZLTable")
+@Slf4j
+@Api(tags="ZL的部门风险系数报表")
+public class ZlRiskController {
+
+    @Autowired
+    ZLService ZLService;
+
+    @GetMapping(value = "SectorRiskFactorTable")
+    @ApiOperation(value = "部门风险系数表")
+    public Result SectorRiskFactor(String userIds, String name, String startTime, String endTime) throws ParseException {
+        return Result.ok(ZLService.SectorRiskFactor(userIds,name,startTime,endTime));
+    }
+
+}
